4aea10c6fe316613113f5ce92d6efb5a7df6035a
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-10-31 14:18:53 -0600

    Add funciones and main file

--- figuasPrincipal.py
+++ figuasPrincipal.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import funciones
+from funciones import circulo, triangulo, rectangulo
 
 dataFile = pd.read_csv("figuras.csv")
 
@@ -8,5 +10,13 @@ areas = []
 perimetros = []
 
 for index, row in dataFile.iterrows():
-	print(f"Fila {index}: Figura = {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 = {row[MEDIDA2]}
+	if row["FIGURA"] == "t":
+		area = triangulo(row["MEDIDA1"], row["MEDIDA2"])
+	elif row["FIGURA"] == "r":
+		area = rectangulo(row["MEDIDA1"], row["MEDIDA2"])
+	elif row["FIGURA"] == "c":
+		area = circulo(row["MEDIDA1"])
+	else:
+		area = 0
+	print(f"Fila {index}: Figura {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 = {row['MEDIDA2']}, Area = {area}")
 
---
+++ funciones.py
@@ -0,0 +1,11 @@
+def triangulo(MEDIDA1, MEDIDA2):
+	area = MEDIDA1 * MEDIDA2 / 2
+	return area
+
+def rectangulo(MEDIDA1, MEDIDA2):
+        area = MEDIDA1 * MEDIDA2
+        return area
+
+def circulo(MEDIDA1):
+	area = 3.1416 * MEDIDA1 ** 2
+	return area
