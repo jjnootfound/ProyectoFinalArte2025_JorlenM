e409829063628023867287fe33bf42675644811c
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-11-01 13:54:02 -0600

    Actualización de código

--- figuasPrincipal.py
+++ figuasPrincipal.py
@@ -1,12 +1,16 @@
+#Importamos los modulos necesarios
+
 import pandas as pd
 import funciones
 from funciones import circulo, triangulo, rectangulo
+
+#Leemos el archivo
 
 dataFile = pd.read_csv("figuras.csv")
 
 print("procesando figuras... \n")
 
-
+#Dependiendo de la figura, se usa la función adecuada y se despliega el valor del area y otros datos
 
 for index, row in dataFile.iterrows():
 	if row["FIGURA"] == "t":
@@ -18,4 +22,3 @@ for index, row in dataFile.iterrows():
 	else:
 		area = 0
 	print(f"Fila {index}: Figura {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 = {row['MEDIDA2']}, Area = {area}")
-
