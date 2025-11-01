bfafabd0a93638966274d45d8bf31447e2170882
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-11-01 17:43:03 -0600

    feat: commented code

--- funciones.py
+++ funciones.py
@@ -1,10 +1,16 @@
+#Función para el área de un triángulo
+
 def triangulo(MEDIDA1, MEDIDA2):
 	area = MEDIDA1 * MEDIDA2 / 2
 	return area
 
+#Función para el área de un rectángulo
+
 def rectangulo(MEDIDA1, MEDIDA2):
         area = MEDIDA1 * MEDIDA2
         return area
+
+#Función para el área de un círculo
 
 def circulo(MEDIDA1):
 	area = 3.1416 * MEDIDA1 ** 2
